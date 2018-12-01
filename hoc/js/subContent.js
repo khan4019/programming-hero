850783fb324558270108697fec26cf9306c4be51
Your Name <AhmmedRasel2@Gmail.com>
2018-12-01 23:18:15 +0600

    assets directory folder changed

--- hoc/js/subContent.js
+++
@@ -1,7 +0,0 @@
-$('#btnL1').on('click',function(){
-    if($('#L1Page1').css('display')!='none'){
-    $('#L1Page2').show().siblings('div').hide();
-    }else if($('#L1Page2').css('display')!='none'){
-        $('#L1Page1').show().siblings('div').hide();
-    }
-});
