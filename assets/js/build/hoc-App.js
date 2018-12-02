3717971976eb84d9c8acd846aefc2fa4319c48a4
Your Name <AhmmedRasel2@Gmail.com>
2018-12-02 20:16:53 +0600

    all planet play icon added

--- assets/js/build/hoc-App.js
+++ assets/js/build/hoc-App.js
@@ -10,66 +10,72 @@ $('.planetMenuIcon').on('click', function () {
 $('#btnPlanet1').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet2,.planet3,.planet4,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide();
         $("#L0img").attr('src', "../assets/img/hoc/planet/completed.png");
         $("#L1img").attr('src', "../assets/img/hoc/planet/start.png");
-        $('.planet2,.planet3,.planet4,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide('slow');
     }
 });
 $('.planet2').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet3,.planet4,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide();
         $("#L1img").attr('src', "../assets/img/hoc/planet/completed.png");
         $("#L2img").attr('src', "../assets/img/hoc/planet/start.png");
-        $('.planet1,.planet3,.planet4,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide('slow');
     }
 });
 $('.planet3').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet2,.planet4,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide();
         $("#L2img").attr('src', "../assets/img/hoc/planet/completed.png");
-        $('.planet1,.planet2,.planet4,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide('slow');
+        $("#L3img").attr('src', "../assets/img/hoc/planet/start.png");
     }
 });
 $('.planet4').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide();
         $("#L3img").attr('src', "../assets/img/hoc/planet/completed.png");
-        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide('slow');
+        $("#L4img").attr('src', "../assets/img/hoc/planet/start.png");
     }
 });
 $('.planet5').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet2,.planet3,.planet4,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide();
         $("#L4img").attr('src', "../assets/img/hoc/planet/completed.png");
-        $('.planet1,.planet2,.planet3,.planet4,.planet6,.planet7,.planet8,.planet9,#btnNextPrev').hide('slow');
+        $("#L5img").attr('src', "../assets/img/hoc/planet/start.png");
     }
 });
 $('.planet6').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet2,.planet3,.planet5,.planet4,.planet7,.planet8,.planet9,#btnNextPrev').hide();
         $("#L5img").attr('src', "../assets/img/hoc/planet/completed.png");
-        $('.planet1,.planet2,.planet3,.planet5,.planet4,.planet7,.planet8,.planet9,#btnNextPrev').hide('slow');
+        $("#L6img").attr('src', "../assets/img/hoc/planet/start.png");
     }
 });
 $('.planet7').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet4,.planet8,.planet9,#btnNextPrev').hide();
         $("#L6img").attr('src', "../assets/img/hoc/planet/completed.png");
-        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet4,.planet8,.planet9,#btnNextPrev').hide('slow');
+        $("#L7img").attr('src', "../assets/img/hoc/planet/start.png");
     }
 });
 $('.planet8').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet7,.planet4,.planet9,#btnNextPrev').hide();
         $("#L7img").attr('src', "../assets/img/hoc/planet/completed.png");
-        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet7,.planet4,.planet9,#btnNextPrev').hide('slow');
+        $("#L8img").attr('src', "../assets/img/hoc/planet/start.png");
     }
 });
 $('.planet9').on('click', function () {
     if ($('.carouselSliderItems').css('display') === 'none') {
         $('.carouselSliderItems').show();
+        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet7,.planet8,.planet4,#btnNextPrev').hide();
         $("#L8img").attr('src', "../assets/img/hoc/planet/completed.png");
-        $('.planet1,.planet2,.planet3,.planet5,.planet6,.planet7,.planet8,.planet4,#btnNextPrev').hide('slow');
     }
 });
 var $finish = $("#btnFinishL1, #btnFinishL2, #btnFinishL3, #btnFinishL4, #btnFinishL5, #btnFinishL6, #btnFinishL7, #btnFinishL8");
