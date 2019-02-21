7491d921507dc8849ec2ff0c29a5eaf395f7825c
Your Name <AhmmedRasel2@Gmail.com>
2019-02-22 01:10:40 +0600

    owl carousel added

---
+++ assets/js/libs/owl_carousel.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[990,2],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:false,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:true,
+        dots:false,
+        nav:false
+    });
+});
