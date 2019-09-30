6aae2fed7fb3cdcb98bd0087830569bedbf61d57
ProRasel <programmerrasel@gmail.com>
2019-09-30 23:48:10 +0600

    video added

---
+++ assets/css/simple-lib-css/video.js
@@ -0,0 +1,8 @@
+
+$('.modal').on('hidden.bs.modal', function(e) {
+    var closestIframe = $(e.currentTarget).find('iframe');
+    var rawVideoURL = $("iframe")[0].src;
+
+    closestIframe[0].src = "";
+    closestIframe[0].src = rawVideoURL;
+  });
